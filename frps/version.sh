28f12c5e4f6111bcb63d79a983dbe40e23fb09eb
小柒 <benny.shen@msn.com>
2024-11-29 16:59:26 +0800

    Add files via upload

---
+++ frps/version.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export FRPS_VER=0.61.0
+export FRPS_INIT="https://hao.iam7.cn/frps/frps.init"
+export aliyun_download_url="https://code.aliyun.com/clangcn/frp/raw/master"
+export github_download_url="https://github.com/fatedier/frp/releases/download"
