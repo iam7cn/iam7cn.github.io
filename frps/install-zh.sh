85a797ea87a0076857ef10fe8d1f9977e97312f8
小柒 <benny.shen@msn.com>
2024-12-04 15:13:04 +0800

    Update install-zh.sh

--- frps/install-zh.sh
+++ frps/install-zh.sh
@@ -19,7 +19,7 @@ program_name="frps"
 version="20231028"
 str_program_dir="/usr/local/${program_name}"
 program_init="/etc/init.d/${program_name}"
-program_config_file="frps.ini"
+program_config_file="frps.toml"
 ver_file="/tmp/.frp_ver.sh"
 str_install_shell="https://hao.iam7.cn/frps/install-zh.sh"
 shell_update(){
