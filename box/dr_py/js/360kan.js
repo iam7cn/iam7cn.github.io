eb6dd28a26955e81c60bf094996e6bc9def55c53
小柒 <benny.shen@msn.com>
2026-05-08 13:45:53 +0800

    Update 360kan.js

--- box/dr_py/js/360kan.js
+++ box/dr_py/js/360kan.js
@@ -1,7 +1,8 @@
 var rule = {
     title:'360影视',
     host:'https://www.360kan.com',
-    homeUrl:'https://api.web.360kan.com/v1/rank?cat=2&size=12',
+	homeUrl:'',
+//    homeUrl:'https://api.web.360kan.com/v1/rank?cat=2&size=12',
     detailUrl:'https://api.web.360kan.com/v1/detail?cat=fyclass&id=fyid',
     searchUrl:'https://api.so.360kan.com/index?force_v=1&kw=**&from=&pageno=fypage&v_ap=1&tab=all',
     url:'https://api.web.360kan.com/v1/filter/list?catid=fyclass&rank=rankhot&cat=&year=&area=&act=&size=35&pageno=fypage&callback=',
