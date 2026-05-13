e307eb818c4b7ddfb24c717198db87174d8ee111
小柒 <benny.shen@msn.com>
2026-05-13 16:57:10 +0800

    Update 555影视[2026].js

--- box/dr_py/js/555影视[2026].js
+++ box/dr_py/js/555影视[2026].js
@@ -1,6 +1,6 @@
 var rule = {
 	title: '555影视[V2]', // csp_AppYsV2
-	host: ' https://555dy1.com/ ',
+	host: ' https://555dy1.com/ ', //https://www.555gy.cc/
 	homeUrl:'/api.php/app/index_video',
 	// url: '/api.php/app/video?tid=fyclass&class=&area=&lang=&year=&limit=20&pg=fypage',
 	url: '/api.php/app/video?tid=fyclassfyfilter&limit=20&pg=fypage',
