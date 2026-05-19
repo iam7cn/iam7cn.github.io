db2242a87a778f2d4aa3558fb38514462cadac3f
小柒 <benny.shen@msn.com>
2026-05-19 08:05:04 +0800

    Update 茶杯狐影视.py

--- box/python/custom/茶杯狐影视.py
+++ box/python/custom/茶杯狐影视.py
@@ -27,7 +27,7 @@ import os
 
 sys.path.append('..')
 
-xurl = "https://cupfoxys.cc"
+xurl = "https://cupfox.love/"
 
 headerx = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36'
